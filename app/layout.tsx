80e4c91881ca011280585d07ac41d59b053dbf75
Alien-hwx <109501324+Alien-hwx@users.noreply.github.com>
2023-07-31 19:16:33 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "AI Chat Web",
+  title: "Alien-AI",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
